863ef3eaca545f3eaea7d4dd7127f3388e151767
mf_binhnd <binh.nd@mediafusion.co.jp>
2021-11-10 16:54:44 +0700

    update travis yaml and deploy script
    Signed-off-by: mf_binhnd <binh.nd@mediafusion.co.jp>

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t ndbinh/multi-client:latest -t ndbinh/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ndbinh/multi-server:latest -t ndbinh/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ndbinh/multi-worker:latest -t ndbinh/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push ndbinh/multi-client:latest
+docker push ndbinh/multi-server:latest
+docker push ndbinh/multi-worker:latest
+
+docker push ndbinh/multi-client:$SHA
+docker push ndbinh/multi-server:$SHA
+docker push ndbinh/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/multi-server server=ndbinh/multi-server:$SHA
+kubectl set image deployments/multi-client client=ndbinh/multi-client:$SHA
+kubectl set image deployments/multi-worker worker=ndbinh/multi-worker:$SHA
